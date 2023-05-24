3fd85aa1c6158b5c93315c6acfa064d05a8dda3d
handsomemin <128664961+handsomeminzz@users.noreply.github.com>
2023-05-24 20:19:07 +0900

    Update Weapon 미완.java

--- Weapon 미완.java
+++ Weapon 미완.java
@@ -19,20 +19,38 @@ public abstract class Weapon {
     }
 }
 
-class WizardWeapon extends Weapon {
-    public WizardWeapon() {
+class WizardWeapon1 extends Weapon {
+    public WizardWeapon1() {
         super("위자드 스테프", 20, 100);
     }
 }
 
-class WarriorWeapon extends Weapon {
-    public WarriorWeapon() {
-        super("청룡 대검", 20, 100);
+class WizardWeapon2 extends Weapon {
+    public WizardWeapon2() {
+        super("드래곤 완드", 30, 200);
     }
 }
 
-class RogueWeapon extends Weapon {
-    public RogueWeapon() {
+class WarriorWeapon1 extends Weapon {
+    public WarriorWeapon1() {
+        super("청룡 대검", 40, 150);
+    }
+}
+
+class WarriorWeapon2 extends Weapon {
+    public WarriorWeapon2() {
+        super("클레이모어", 60, 250);
+    }
+}
+
+class RogueWeapon1 extends Weapon {
+    public RogueWeapon1() {
         super("포이즌 단검", 25, 110);
+    }
+}
+
+class RogueWeapon2 extends Weapon {
+    public RogueWeapon2() {
+        super("듀얼블레이드", 35, 210);
     }
 }
