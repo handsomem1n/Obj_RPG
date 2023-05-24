75db75bcab4933977c486bcb60ad48dca83ce0f8
handsomemin <128664961+handsomeminzz@users.noreply.github.com>
2023-05-24 19:01:55 +0900

    Update WeaponShop미완.java

--- WeaponShop미완.java
+++ WeaponShop미완.java
@@ -43,16 +43,16 @@ public class WeaponShop {
 /*메인함수에서 실행
 public class Main {
     public static void main(String[] args) {
-        WeaponStore weaponStore = new WeaponStore();
-        weaponStore.addWeapon(new WizardWeapon());
-        weaponStore.addWeapon(new WarriorWeapon());
-        weaponStore.addWeapon(new RogueWeapon());
+        WeaponShop weaponStore = new WeaponStore();
+        weaponShop.addWeapon(new WizardWeapon());
+        weaponShop.addWeapon(new WarriorWeapon());
+        weaponShop.addWeapon(new RogueWeapon());
 
         Hero hero = new Hero(); // (히어로 돈 받아오기)
 
-        weaponStore.displayWeapons();
-        weaponStore.buyWeapon(hero, "청룡 대검");
-        weaponStore.buyWeapon(hero, "위자드 스테프");
+        weaponShop.displayWeapons();
+        weaponShop.buyWeapon(hero, "청룡 대검");
+        weaponShop.buyWeapon(hero, "위자드 스테프");
     }
 }
 
