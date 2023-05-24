6a65138f4d47cc265cc41f72c62ec8ef39c6a424
handsomemin <128664961+handsomeminzz@users.noreply.github.com>
2023-05-24 12:42:08 +0900

    Update WeaponShop.java

--- WeaponShop.java
+++ WeaponShop.java
@@ -11,22 +11,49 @@ public class WeaponShop {
         this.weapons.add(weapon);
     }
 
+    public void buyWeapon(Hero hero, String weaponName) {
+        Weapon weaponToBuy = null;
+        for (Weapon weapon : weapons) {
+            if (weapon.getName().equals(weaponName)) {
+                weaponToBuy = weapon;
+                break;
+            }
+        }
+
+        if (weaponToBuy != null) {
+            if (hero.getMoney() >= weaponToBuy.getPrice()) {
+                hero.setMoney(hero.getMoney() - weaponToBuy.getPrice());
+                System.out.println(weaponName + " 구매 성공! 잔여 액수: " + hero.getMoney());
+            } else {
+                System.out.println("구매 실패! " + weaponName);
+            }
+        } else {
+            System.out.println("찾을 수 없음..");
+        }
+    }
+
     public void displayWeapons() {
         for (Weapon weapon : weapons) {
-            System.out.println("Weapon Name: " + weapon.getName() + ", Damage: " + weapon.getDamage());
+            System.out.println("무기 이름: " + weapon.getName() + ", 데미지: " + weapon.getDamage() + ", 금액: " + weapon.getPrice());
         }
     }
 }
-/*
+
+
+/*메인함수에서 실행
 public class Main {
     public static void main(String[] args) {
-        WeaponShop weaponShop = new WeaponShop();
+        WeaponStore weaponStore = new WeaponStore();
+        weaponStore.addWeapon(new WizardWeapon());
+        weaponStore.addWeapon(new WarriorWeapon());
+        weaponStore.addWeapon(new RogueWeapon());
 
-        weaponShop.addWeapon(new WizardWeapon());
-        weaponShop.addWeapon(new WarriorWeapon());
-        weaponShop.addWeapon(new RogueWeapon());
+        Hero hero = new Hero(); // (히어로 돈 받아오기)
 
-        weaponShop.displayWeapons();
+        weaponStore.displayWeapons();
+        weaponStore.buyWeapon(hero, "청룡 대검");
+        weaponStore.buyWeapon(hero, "위자드 스테프");
     }
 }
+
 */
