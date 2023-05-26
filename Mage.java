3d2f78a3ff9f17d795d94b94d7eb9bb6a570aadf
handsomemin <128664961+handsomeminzz@users.noreply.github.com>
2023-05-26 11:04:05 +0900

    Create Mage.java

---
+++ Mage.java
@@ -0,0 +1,12 @@
+// package SOandRU;
+
+public class Mage extends Hero {
+    public Mage(String name) {
+        super(name);
+        this.power = 20;
+        this.hp = 50;
+        this.defense = 10;
+        this.mp = 30;
+        this.experience = 0;
+    }
+}
