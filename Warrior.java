96d640261ef2976229c3bb13280331faf4b2fd8d
handsomemin <128664961+handsomeminzz@users.noreply.github.com>
2023-05-26 11:04:38 +0900

    Create Warrior.java

---
+++ Warrior.java
@@ -0,0 +1,12 @@
+// package SOandRU;
+
+public class Warrior extends Hero {
+    public Warrior(String name) {
+        super(name);
+        this.power = 15;
+        this.hp = 90;
+        this.defense = 30;
+        this.mp = 10;
+        this.experience = 0;
+    }
+}
