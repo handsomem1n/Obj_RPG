2e8c92ac98cb3fd9070d0676b8d7286f2bd8afd3
handsomemin <128664961+handsomeminzz@users.noreply.github.com>
2023-05-26 11:04:21 +0900

    Create Rogue.java

---
+++ Rogue.java
@@ -0,0 +1,12 @@
+// package SOandRU;
+
+public class Rogue extends Hero {
+    public Rogue(String name) {
+        super(name);
+        this.power = 25;
+        this.hp = 60;
+        this.defense = 15;
+        this.mp = 10;
+        this.experience = 0;
+    }
+}
