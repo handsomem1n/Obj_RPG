b7ebd5da3ee4fead965d264e360de5b0566e7734
handsomemin <128664961+handsomeminzz@users.noreply.github.com>
2023-05-29 20:41:43 +0900

    Update Warrior.java

--- src/soandruRPG/Warrior.java
+++ src/soandruRPG/Warrior.java
@@ -11,19 +11,20 @@ public class Warrior extends Hero {
 		this.experience = 0;
 	}
 
-	public int attack(int num) {
-		switch (num) {
+	public int attack(int skillNum) {
+		switch (skillNum) {
 		default:
 		case 1:
 			// 기본 공격
+			System.out.printf("%s의 기본 공격\n", this.name);
 			return (this.level * 10) + (this.power * 20);
 
 		case 2: // 스킬1
-			System.out.println("슬래시 블러스트");
+			System.out.printf("%s의 슬래시 블러스트!!\n", this.name);
 			return (this.level * 15) + (this.power * 25);
 
 		case 3: // 스킬2
-			System.out.println("브레이브 슬래시");
+			System.out.printf("%s의 브랜디쉬!!\n", this.name);
 			return (this.level * 20) + (this.power * 30);
 		}
 	}
