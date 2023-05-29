b2e8dd5f4d8cca3609fb562cb80415c29ece8028
handsomemin <128664961+handsomeminzz@users.noreply.github.com>
2023-05-29 20:41:08 +0900

    Update Mage.java

--- src/soandruRPG/Mage.java
+++ src/soandruRPG/Mage.java
@@ -11,19 +11,20 @@ public class Mage extends Hero {
 		this.experience = 0;
 	}
 
-	public int attack(int num) {
-		switch (num) {
+	public int attack(int skillNum) {
+		switch (skillNum) {
 		default:
 		case 1:
 			// 기본 공격
+			System.out.printf("%s의 기본 공격!", this.name);
 			return (this.level * 10) + (this.power * 20);
 
 		case 2: // 스킬1
-			System.out.println("파이어 볼");
+			System.out.printf("%s의 파이어 볼!!", this.name);
 			return (this.level * 15) + (this.power * 30);
 
 		case 3: // 스킬2
-			System.out.println("불기둥");
+			System.out.printf("%s의 불기둥!!", this.name);
 			return (this.level * 20) + (this.power * 40);
 		}
 	}
