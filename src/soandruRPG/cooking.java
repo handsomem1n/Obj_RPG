30a4d3359e938a529a008411e6d4f5f3cbd298a5
ss090055 <132585956+ss090055@users.noreply.github.com>
2023-05-29 09:44:26 +0900

    Update cooking.java

--- src/soandruRPG/cooking.java
+++ src/soandruRPG/cooking.java
@@ -7,16 +7,16 @@ class Cooking {
     public void cook(String ingredient) {
         if (ingredient.equals("너구리의 꼬리")) {
             System.out.println("너구리 스튜를 만들었습니다!");
-//             heroHp += 10;
+//             heroHp += 5;
         } else if (ingredient.equals("살쾡이의 살코기")) {
             System.out.println("살쾡이 스테이크를 만들었습니다!");
-//             heroHp += 15;
+//             heroHp += 10;
         } else if (ingredient.equals("리본돼지의 삼겹살")) {
             System.out.println("삼겹살 구이를 만들었습니다!");
-//             heroHp += 20;
+//             heroHp += 15;
         } else if (ingredient.equals("슬라임의 슬라임")) {
             System.out.println("슬라임 젤리를 만들었습니다!");
-//             heroHp += 5;
+//             heroHp += 20;
         } else {
             System.out.println("잘못된 재료입니다.");
             return;
