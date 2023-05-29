022a1d683179d0e776190a0481521f7d74a3e06a
handsomemin <128664961+handsomeminzz@users.noreply.github.com>
2023-05-29 15:54:16 +0900

    Update Rogue.java

--- src/soandruRPG/Rogue.java
+++ src/soandruRPG/Rogue.java
@@ -1,13 +1,31 @@
-package soandruRPG;
-// package SOandRU;
+package SOandRU;
 
 public class Rogue extends Hero {
-    public Rogue(String name) {
-        super(name);
-        this.power = 25;
-        this.hp = 60;
-        this.defense = 15;
-        this.mp = 10;
-        this.experience = 0;
-    }
+	public Rogue(String name) {
+		super(name);
+		this.power = 25;
+		this.hp = 60;
+		this.defense = 15;
+		this.mp = 10;
+		this.experience = 0;
+	}
+
+	public int attack(int num) {
+		switch (num) {
+		default:
+		case 1:
+			// 기본 공격
+			return (this.level * 10) + (this.power * 20);
+
+		case 2: // 스킬1
+			System.out.println("토네이도 스핀");
+			return (this.level * 15) + (this.power * 30);
+
+		case 3: // 스킬2
+			System.out.println("베놈 슬래시");
+			return (this.level * 20) + (this.power * 50);
+		}
+
+	}
+
 }
