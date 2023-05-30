432571d5c1a132edf03cdd614c603409b2e476cc
handsomemin <128664961+handsomeminzz@users.noreply.github.com>
2023-05-30 19:12:18 +0900

    Update HuntingGround.java

--- src/soandruRPG/HuntingGround.java
+++ src/soandruRPG/HuntingGround.java
@@ -28,32 +28,31 @@ public class HuntingGround {
 	public void battle(Hero h, Monster m) {
 		Scanner sc = new Scanner(System.in);
 		while (true) {
+			System.out.println("*********************");
 			System.out.println(h.name + "의 공격입니다.");
 			// m.monster_attacked(h.defense,h.attack());
 			System.out.println("1번: 기본공격 ");
 			System.out.println("2번: 스킬1 ");
 			System.out.println("3번: 스킬2 ");
-			System.out.print("사용하려는 스킬을 선택해주세요");
+			System.out.print("사용하려는 스킬을 선택해주세요 : ");
 			int skillNum = sc.nextInt();
 			heroAttack(m, h.attack(skillNum));
 			if (m.monster_hp <= 0) {
-
 				System.out.println(m.monster_name + "가 죽었습니다.");
 				h.experience += m.monster_experience;
 				h.money += m.monster_money;
 				h.inventory.put(m.item, h.inventory.get(m.item) != null ? h.inventory.get(m.item) + 1 : 0 + 1);
 				break;
 			}
-
+			System.out.println("*********************");
 			System.out.println(m.monster_name + "의 공격입니다.");
 //			hero_attacked(m.monster_attack());
 			monster_attacked(h, m.monster_attack());
 			if (h.hp <= 0) {
-
 				System.out.println(h.name + "이 죽었습니다.");
 				h.hp = 1;
 				System.out.println(h.name + "이 체력" + h.hp + "으로 부활했습니다.");
-				System.out.println("***************************************");
+				System.out.println("*********************");
 				break;
 			}
 		}
